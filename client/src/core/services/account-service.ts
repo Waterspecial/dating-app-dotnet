a46051fa00cf13e6f2f7c268cef171bf363caea1
Waterspecial <MAC@MacBook-Pro.isdads.salford.ac.uk>
2026-02-15 17:53:18 +0000

    connecting register component to an api

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -11,22 +11,33 @@ export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
   currentUser = signal<User | null>(null);
 
-  login(creds: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
-      tap(
-        user => {
-          if(user) {
-            localStorage.setItem('user', JSON.stringify(user));
-            this.currentUser.set(user)
-          }
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap((user) => {
+        if (user) {
+       this.setCurrentUser(user);
         }
-      )
-    )
+      })
+    );
   }
 
-  logout(){
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap((user) => {
+        if (user) {
+       this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
     localStorage.removeItem('user');
     this.currentUser.set(null);
   }
-
 }
